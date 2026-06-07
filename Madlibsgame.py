6054dcb409551538b8e4fea7ebe2db7b70253ae3
kaustavG1T <kpworks12@gmail.com>
2026-06-08 05:02:17 +0530

    Remove Madlibsgame.py

--- Madlibsgame.py
+++
@@ -1,8 +0,0 @@
-adjective1 = input("enter adjective(description)- ")
-noun1 = input("enter noun(person,place,thing,etc)- ")
-adjective2 = input("enter adjective(description)- ")
-verb1 = input("enter verb ending with 'ing'- ")
-
-print(f"yesterday we went to a {adjective1} restaurant")
-print(f"i saw a {noun1} at the museum")
-print(f"{noun1} was {adjective2} and {verb1}")
